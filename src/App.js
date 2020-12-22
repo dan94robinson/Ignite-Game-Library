65c3774a916ab6d99dc66fd73b88269766d40daf
Daniel Robinson <69058667+dan94robinson@users.noreply.github.com>
2020-12-22 11:42:06 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//Components and pages
+import Home from './pages/Home';
+import Nav from './components/Nav';
+//Styles
+import GlobalStyles from './components/GlobalStyles';
+//Route
+import {Route} from 'react-router-dom';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Nav />
+      <Route path={['/game/:id', '/']}>
+        <Home />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
